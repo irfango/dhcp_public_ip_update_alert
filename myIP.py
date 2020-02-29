b75b20025c9305ff85acbd6a289e19f37c6e3144
Ahmed Shan <ahmedshan16@gmail.com>
2020-02-29 05:34:27 +0500

    Added a method to create myCurrentIP on the go
    Added friendly comments

--- myIP.py
+++ myIP.py
@@ -7,6 +7,9 @@ import urllib.request
 import time
 import smtplib
 import os
+import os.path
+from os import path
+from shutil import copyfile
 
 # Defining Base Path
 basePath = os.path.dirname(os.path.abspath(__file__))
@@ -14,28 +17,39 @@ basePath = os.path.dirname(os.path.abspath(__file__))
 # How do you want to be alerted?
 alert_method = 'both' # Options: both / telegram / email
 
-#Email Alert Details Gmail.
+# Email Alert Details Gmail.
 SENDER = "SENDER EMAIL HERE"
 PASS = "SENDER PASSWORD HERE"
 RECIVER = "RECIVER EMAIL HERE"
 
+# Set telegram chat id
 TELEGRAM_CHAT_ID = "TELEGRAM CHAT ID"
 
 bot = telegram_chatbot(basePath + "/telegram_config.cfg")
 
+# Fetch current IP address from the WWW
 FetchURL = urllib.request.urlopen("https://ident.me/")
 Data = FetchURL.read()
-
 myIP = Data.decode("utf8")
 FetchURL.close()
 
+# Set the current IP file
 currentIPFile = basePath + "/myCurrentIP.txt"
+
+# Check if file exists, if not create a file... This file has been .ignored
+if path.exists(currentIPFile) == False:
+    # Create a copy from the sample file
+    copyfile(basePath + "/myCurrentIP.sample", currentIPFile)
+
 
 FILE = open(currentIPFile, "r")
 CurrentIP = FILE.read()
 
+# If IP did not change
 if myIP == CurrentIP:
     print("Its all good")
+
+# If IP Changed
 elif myIP != CurrentIP:
     oldIP = CurrentIP
     print("IP is different, update is in progress")
@@ -44,15 +58,14 @@ elif myIP != CurrentIP:
     print("IP Updated")
     FILE = open(currentIPFile, "r")
     CurrentIP = FILE.read()
-    #time.sleep(1)
     print("Current IP is " + CurrentIP)
     FILE.close()
 
-    # Telegram Bot Sends Message
+    # Send a notification message from TELEGRAM BOT, if option is 'both' or 'telegram'
     if alert_method == 'both' or alert_method == 'telegram':
         bot.send_message("IP updated from " + oldIP + " to " + CurrentIP, TELEGRAM_CHAT_ID)
 
-    #Email Alert
+    # Send a notification message from EMAIL, if option is 'both' or 'email'
     if alert_method == 'both' or alert_method == 'email':
         server = smtplib.SMTP('smtp.gmail.com', 587)
         server.ehlo()
