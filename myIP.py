1df2ea59a5de0fa703a462e816d3406d8f197789
Ahmed Shan <ahmedshan16@gmail.com>
2020-02-29 05:23:22 +0500

    Its Working.
    1. Fixed an issue running the script as a cron job
    
    2. Added a method to choose whether to send alert via Telegram, Email or Both

--- myIP.py
+++ myIP.py
@@ -11,12 +11,15 @@ import os
 # Defining Base Path
 basePath = os.path.dirname(os.path.abspath(__file__))
 
+# How do you want to be alerted?
+alert_method = 'both' # Options: both / telegram / email
+
 #Email Alert Details Gmail.
 SENDER = "SENDER EMAIL HERE"
 PASS = "SENDER PASSWORD HERE"
 RECIVER = "RECIVER EMAIL HERE"
 
-TELEGRAM_CHAT_ID = "RECIVER TELEGRAM CHAT_ID HERE"
+TELEGRAM_CHAT_ID = "TELEGRAM CHAT ID"
 
 bot = telegram_chatbot(basePath + "/telegram_config.cfg")
 
@@ -25,38 +28,41 @@ Data = FetchURL.read()
 
 myIP = Data.decode("utf8")
 FetchURL.close()
-print(myIP)
 
+currentIPFile = basePath + "/myCurrentIP.txt"
 
-FILE = open("myCurrentIP.txt", "r")
+FILE = open(currentIPFile, "r")
 CurrentIP = FILE.read()
-print(CurrentIP)
+
 if myIP == CurrentIP:
     print("Its all good")
 elif myIP != CurrentIP:
+    oldIP = CurrentIP
     print("IP is different, update is in progress")
-    FILE = open("myCurrentIP.txt", "w")
+    FILE = open(currentIPFile, "w")
     FILE.write(myIP)
     print("IP Updated")
-    FILE = open("myCurrentIP.txt", "r")
+    FILE = open(currentIPFile, "r")
     CurrentIP = FILE.read()
     #time.sleep(1)
     print("Current IP is " + CurrentIP)
     FILE.close()
 
     # Telegram Bot Sends Message
-    bot.send_message(myIP, TELEGRAM_CHAT_ID)
+    if alert_method == 'both' or alert_method == 'telegram':
+        bot.send_message("IP updated from " + oldIP + " to " + CurrentIP, TELEGRAM_CHAT_ID)
 
     #Email Alert
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.ehlo()
-    server.starttls()
-    server.login(SENDER, PASS)
-    subject = 'DHCP Public IP Updated'
-    msg = CurrentIP
-    message = 'Subject: %s\n\n%s' % (subject, msg)
-    server.sendmail(SENDER, RECIVER, message)
-    server.quit()
-    print("Email Alert Sent!")
+    if alert_method == 'both' or alert_method == 'email':
+        server = smtplib.SMTP('smtp.gmail.com', 587)
+        server.ehlo()
+        server.starttls()
+        server.login(SENDER, PASS)
+        subject = 'DHCP Public IP Updated'
+        msg = CurrentIP
+        message = 'Subject: %s\n\n%s' % (subject, msg)
+        server.sendmail(SENDER, RECIVER, message)
+        server.quit()
+        print("Email Alert Sent!")
 
 
